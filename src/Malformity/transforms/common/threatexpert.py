a2fe065b9df0e15db60a38e25c673e416706425d
allfro <ndouba@gmail.com>
2012-12-21 10:46:17 -0500

    Changed sys.exit() to MaltegoException
    Same as patch-1

--- src/Malformity/transforms/common/threatexpert.py
+++ src/Malformity/transforms/common/threatexpert.py
@@ -3,6 +3,7 @@
 import mechanize
 from BeautifulSoup import BeautifulSoup
 from canari.framework import configure #, superuser
+from canari.maltego.message import MaltegoException
 
 __author__ = 'Keith Gilbert - @digital4rensics'
 __copyright__ = 'Copyright 2012, Malformity Project'
@@ -29,6 +30,6 @@ def build(hash):
 		html = report.read()
 		page = BeautifulSoup(html)
 	except:
-		sys.exit("Error building URL.")
+		raise MaltegoException("Error building URL.")
 		
 	return page
